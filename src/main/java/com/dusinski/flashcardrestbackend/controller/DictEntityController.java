22a5c7dd40441f36f7111304a0211c88552e282a
ddusinski <dominik.dusinski@gmail.com>
2021-05-17 14:07:17 +0200

    Adding static content to main page

--- src/main/java/com/dusinski/flashcardrestbackend/controller/DictEntityController.java
+++ src/main/java/com/dusinski/flashcardrestbackend/controller/DictEntityController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/dict")
+@RequestMapping("/api/dict")
 public class DictEntityController {
 
     @Autowired
--- src/main/java/com/dusinski/flashcardrestbackend/controller/FlashcardController.java
+++ src/main/java/com/dusinski/flashcardrestbackend/controller/FlashcardController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/flashcard")
+@RequestMapping("/api/flashcard")
 public class FlashcardController {
 
     @Autowired
